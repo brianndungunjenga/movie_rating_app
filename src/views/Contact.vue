c6049b668c0f0171a8c9725c8edc6c7a0c7d4509
brianndungunjenga <ndungunjenga96@gmail.com>
2019-10-25 11:56:57 +0300

    added Contact view

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout class="contact">
+    <HelloWorld msg="This is our contact page"/>
+  </v-layout>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'contact',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
