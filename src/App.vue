b731c12d7ba6b441a2a8b8fee8c425ad052b6aa0
brianndungunjenga <ndungunjenga96@gmail.com>
2019-10-25 12:02:37 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+
+    <v-navigation-drawer
+
+      permanent
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <router-link v-bind:to="{ name: 'home' }" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>Home</v-list-item-content>
+          </v-list-item>
+        </router-link>
+        <router-link v-bind:to="{ name: 'contact' }" class="side_bar_link">
+          <v-list-item>
+            <v-list-item-action>
+              <v-icon>contact_mail</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>Contact</v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app color="indigo" dark fixed >
+      <v-app-bar-nav-icon ></v-app-bar-nav-icon>
+      <v-toolbar-title>Home</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <router-view/>
+        </div>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import './assets/stylesheets/main.css';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  data: () => ({
+    drawer: null,
+  }),
+  props: {
+    source: String,
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
+
 import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from './App.vue';
 import router from './router';
+import vuetify from './plugins/vuetify';
+
+Vue.use(BootstrapVue);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  // vuetify: new Vuetify(),
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
