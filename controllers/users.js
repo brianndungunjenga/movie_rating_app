cc9c826d36d24aa1eb756c77ccedfdbcfbc51dcc
brianndungunjenga <ndungunjenga96@gmail.com>
2019-11-06 21:08:53 +0300

    added API endpoint to register a user

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+/* eslint-disable prefer-destructuring */
+const User = require('../models/User.js');
+
+module.exports.container = (app) => {
+  // register a User
+  app.post('/users/register', (request, response) => {
+    // eslint-disable-next-line prefer-destructuring
+    const name = request.body.name;
+    const email = request.body.email;
+    const password = request.body.password;
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+    User.createUser(newUser, (error, user) => {
+      if (error) {
+        console.log(error);
+      }
+      response.send({ user });
+    });
+  });
+};
